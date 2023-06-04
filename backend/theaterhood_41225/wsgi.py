fd6227805ee384cffdbebb2a02da6ca91247f563
Crowdbotics <team@crowdbotics.com>
2023-06-04 01:14:22 +0000

    Initial commit.

---
+++ backend/theaterhood_41225/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for theaterhood_41225 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'theaterhood_41225.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "theaterhood-41225",
+  url: "https://theaterhood-41225.botics.co",
+  api: "https://theaterhood-41225.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
